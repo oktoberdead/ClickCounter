3d2afed3350b6e2bb81977b9936b348abd7b9c26
oktoberdead <78501382+oktoberdead@users.noreply.github.com>
2021-02-09 15:25:56 +0300

    Add files via upload

---
+++ clickJs.js
@@ -0,0 +1,49 @@
+
+    let but = document.getElementById("button");
+    let cnt = document.getElementById("count");
+    let dim = document.getElementById("dimmer");
+
+    but.style.left = (window.innerWidth-200)/2 + "px";
+    but.style.top = (window.innerHeight-75)/2 + "px";
+    
+
+    cnt.style.left = (window.innerWidth-60)/2 + "px";
+    cnt.style.top = (window.innerHeight+75)/2 +  "px";
+  
+   let clicks = 0; 
+   
+   let canStart = 1;
+   let isStarted = 0;
+   
+   but.onclick = function(){
+       if(canStart && !isStarted){
+           canStart = 0;
+           isStarted = 1;
+           clicks++;
+           but.style.background = "lime";
+           setTimeout(function (){
+           
+           isStarted = 0;
+           but.style.background = "red";
+              setTimeout (function (){
+                  canStart = 1;
+                  clicks = 0;
+                  
+              }, 1500);
+           }, 5000);
+       } else if(isStarted && !canStart ) clicks++;
+       
+       cnt.innerHTML = "";
+       cnt.innerHTML = clicks;
+       
+   } 
+   
+but.onmousedown = function(){
+	dim.style.opacity = 0.3;
+}
+
+but.onmouseup = function(){
+	dim.style.opacity = 0;
+}
+
+
